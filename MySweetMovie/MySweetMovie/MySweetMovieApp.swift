17a9761f0b21afc3471b7b11c487c5ca610288a3
Tri Bui Q. VN.Hanoi <tri.bui@monstar-lab.com>
2023-02-17 10:03:57 +0700

    create project

---
+++ MySweetMovie/MySweetMovie/MySweetMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySweetMovieApp.swift
+//  MySweetMovie
+//
+//  Created by Tri Bui Q. VN.Hanoi on 17/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct MySweetMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
